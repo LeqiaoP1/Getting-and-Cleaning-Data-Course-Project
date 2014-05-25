c05a23a04548152ad6e2c54588ed461628c83048
LeqiaoP1 <leqiaop1@sina.com>
2014-05-25 18:25:16 +0200

    update

--- run_analysis.R
+++ run_analysis.R
@@ -8,24 +8,24 @@ library(reshape2)
 
 ## ----------------- load lables for "activity" and "feature" ---------------
 ## Read all activities and their names and label the aproppriate columns 
-activity_labels <- read.table("./activity_labels.txt",col.names=c("activity_id","activity_name"))
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt",col.names=c("activity_id","activity_name"))
 
 ## Read the dataframe's column names
-feature_lables <- read.table("features.txt", col.names=c("feature_id", "feature_name"))
+feature_lables <- read.table("./UCI HAR Dataset/features.txt", col.names=c("feature_id", "feature_name"))
 feature_names <-  feature_lables[,2]
 
 
 ## ----------------- for the test data set --------------------------------
 ## Read the test data and label the dataframe's columns
-testdata <- read.table("./test/X_test.txt")
+testdata <- read.table("./UCI HAR Dataset/test/X_test.txt")
 colnames(testdata) <- feature_names
 
 ## Read the ids of the test subjects and label the the dataframe's columns
-test_subject_id <- read.table("./test/subject_test.txt")
+test_subject_id <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 colnames(test_subject_id) <- "subject_id"
 
 ## Read the activity id's of the test data and label the the dataframe's columns
-test_activity_id <- read.table("./test/y_test.txt")
+test_activity_id <- read.table("./UCI HAR Dataset/test/y_test.txt")
 colnames(test_activity_id) <- "activity_id"
 
 
@@ -36,16 +36,16 @@ test_data <- cbind(test_subject_id , test_activity_id , testdata)
 
 ## ------------ for the train data set ------------------------------------    
 ## Read the training data and label the dataframe's columns
-traindata <- read.table("./train/X_train.txt")
+traindata <- read.table("./UCI HAR Dataset/train/X_train.txt")
 colnames(traindata) <- feature_names
 
 ## Read the ids of the test subjects and label the the dataframe's columns
-train_subject_id <- read.table("./train/subject_train.txt")
+train_subject_id <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 colnames(train_subject_id) <- "subject_id"
 
 ## Read the activity id's of the training data and label 
 ##the dataframe's columns
-train_activity_id <- read.table("./train/y_train.txt")
+train_activity_id <- read.table("./UCI HAR Dataset/train/y_train.txt")
 colnames(train_activity_id) <- "activity_id"
 
 ## train data into one dataframe
@@ -68,7 +68,7 @@ mean_std_data <-all_data[,c("subject_id","activity_id",mean_col_names,std_col_na
 descrnames <- merge(activity_labels, mean_std_data, by="activity_id", all=TRUE)
 
 ## delete the "id" because we have already descriptives
-data_melt$activity_id <- NULL
+descrnames$activity_id <- NULL
 
 
 ## --------------- output tidy data set ---------------------------
@@ -83,4 +83,4 @@ output_data <- dcast(data_melt, activity_name + subject_id ~ variable, mean)
 ## Create a file with the new tidy dataset
 write.table(output_data,"./tidy_movement_data.txt")
 
-
+## DONE!
